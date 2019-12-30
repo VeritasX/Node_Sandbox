9a696534275ad63a6a040fca86ec6214cfa044e9
Aaron Endsley <aaronendsley@gmail.com>
2019-12-30 12:01:43 -0600

    added the ability for the success page to get the last submitted items title through req.params.id

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
 const Entry = mongoose.model('Entry');
 
-exports.mainPage = async (req, res) => {
+const findEntryAndReturn = async () => {
   let data = await Entry.find();
   data = data.sort((a, b) => (a.created < b.created ? 1 : -1));
-  console.log(data);
-  res.render('mpContent', { title: 'We are super cool', data });
+  return data;
+};
+exports.mainPage = async (req, res) => {
+  const data = await findEntryAndReturn();
+  res.render('mpContent', {
+    title: 'Enter A Game or movie',
+    sentence: 'Enter in your title and description (we will add more data to this as we build our site)',
+    data
+  });
 };
 
 exports.mainPageSubmit = async (req, res) => {
   req.body.browser = req.headers['user-agent'];
   req.body.created = new Date().getTime();
-  console.log(req.body.created);
   const store = await new Entry(req.body).save();
+  res.redirect(`/success/${req.body.name}`);
+};
+
+exports.success = async (req, res) => {
+  const data = await findEntryAndReturn();
+  res.render('mpContent', {
+    title: `Success! You have submitted "${req.params.name}"`,
+    sentence: 'if you would like to add another movie or game please make another submission',
+    data
+  });
+};
+
+exports.redirectSuccess = (req, res) => {
   res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const mainRoutes = require('../controllers/mainController');
 
 router.get('/', mainRoutes.mainPage);
 router.post('/', mainRoutes.mainPageSubmit);
+router.get('/success/:name', mainRoutes.success);
+router.post('/success/:name', mainRoutes.success);
+router.get('/success', mainRoutes.redirectSuccess);
 
 module.exports = router;
